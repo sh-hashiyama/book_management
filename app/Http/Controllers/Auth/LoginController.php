b9069e3feb71c9e75f718034360149e85d45af2e
sh-hashiyama <sh-hashiyama@city.co.jp>
2019-08-19 21:36:03 +0900

    [fix] 不要なコードを削除

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -61,12 +61,6 @@ class LoginController extends Controller
     public function handleGoogleCallback()
     {
         $gUser = Socialite::driver('google')->stateless()->user();
-        // email が合致するユーザーを取得
-        // $user = User::where('email', $gUser->email)->first();
-        // // 見つからなければ新しくユーザーを作成
-        // if ($user == null) {
-        //     $user = $this->createUserByGoogle($gUser);
-        // }
         $user = $this->userService->getUserByGoogleAccount($gUser);
         // ログイン処理
         \Auth::login($user, true);
